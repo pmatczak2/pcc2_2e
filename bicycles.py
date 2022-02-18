1ebb4b98703c757077fc735157e8475ba7fdb760
peteykienzzz <pmatczak2@gmail.com>
2022-02-18 04:49:24 -0800

    working with lists

--- bicycles.py
+++ bicycles.py
@@ -4,3 +4,18 @@ print(bicycles)
 # Accessing items in a list.
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles[0])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+
+# Index position starts at 0, Not 1.
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
+print(bicycles[3])
+
+# Using Individual Values from a List
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+
+# Using individual Values from a List
+
