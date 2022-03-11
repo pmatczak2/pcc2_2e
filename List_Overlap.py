1d44bb26a94a46fb8974ce8aaee070f4edb054b8
peteykienzzz <pmatczak2@gmail.com>
2022-03-10 16:45:06 -0800

    return a list that contains only the elements that are common

---
+++ List_Overlap.py
@@ -0,0 +1,13 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+for i in a:
+    if i in b and i not in c:
+        c.append(i)
+print(c)
+
+banned_users = ['ann', 'chad', 'dee']
+user = 'erin'
+if user not in banned_users:
+    print("You can play!")
