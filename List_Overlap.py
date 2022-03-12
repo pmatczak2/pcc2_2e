7763e85d91d617a88ace7093814d5002fd6cba94
peteykienzzz <pmatczak2@gmail.com>
2022-03-11 16:23:50 -0800

    made minor change

--- List_Overlap.py
+++ List_Overlap.py
@@ -1,13 +1,9 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = []
-
-for i in a:
-    if i in b and i not in c:
-        c.append(i)
+c = list(set([x for x in a if x in b]))
 print(c)
 
-banned_users = ['ann', 'chad', 'dee']
-user = 'erin'
-if user not in banned_users:
-    print("You can play!")
+# banned_users = ['ann', 'chad', 'dee']
+# user = 'erin'
+# if user not in banned_users:
+#     print("You can play!")
