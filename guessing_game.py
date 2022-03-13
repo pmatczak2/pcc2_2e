7377c5860e42485ea786cc73f4d5afbe54b6f687
peteykienzzz <pmatczak2@gmail.com>
2022-03-13 12:08:27 -0700

    guessing game

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number,
+# then tell them whether they guessed too low, too high, or exactly right.
+
+import random
+guesses = 0
+mystery_number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    guesses += 1
+    if guess == mystery_number:
+        print("you guessed the number just right")
+        print(f"It took you guesses {guesses}")
+        break
+    elif guess < mystery_number:
+        print("your guess is too low")
+    else:
+        print("you guessed too high")
+
+
+
+
+
