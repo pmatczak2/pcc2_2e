4498aaaf7f12a66d21ec428040dcec25fb861f9d
peteykienzzz <pmatczak2@gmail.com>
2022-03-20 11:28:51 -0700

    program that takes a list of numbers ist of only the first and last elements of the given list

--- end_list.py
+++ end_list.py
@@ -1,8 +1,19 @@
 # Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the
 # first and last elements of the given list. For practice, write this code inside a function.
 
-a = [5, 10, 15, 20, 25]
-
+a_list = [5, 10, 15, 20, 25]
 
 ans = a[::len(a)-1]
 print(ans)
+
+
+
+first_last = [a_list[n] for n in (0, -1)]
+print(first_last)
+
+
+
+def list_ends(a_list):
+    return [a_list[0], a_list[len(a_list) - 1]]
+
+print(list_ends(a_list))
