2bd4691df1d509c4db9d9eb03363f71f8d80afea
peteykienzzz <pmatczak2@gmail.com>
2022-03-20 11:34:45 -0700

    program that takes a list of numbers ist of only the first and last elements of the given list

--- end_list.py
+++ end_list.py
@@ -3,17 +3,14 @@
 
 a_list = [5, 10, 15, 20, 25]
 
-ans = a[::len(a)-1]
+
+ans = a_list[::len(a_list)-1]  # list slicing
 print(ans)
 
-
-
-first_last = [a_list[n] for n in (0, -1)]
+first_last = [a_list[n] for n in (0, -1)]  # a list comprehension
 print(first_last)
 
-
-
 def list_ends(a_list):
-    return [a_list[0], a_list[len(a_list) - 1]]
+    return [a_list[0], a_list[len(a_list) - 1]]  # a function returning first and last item in a list
 
 print(list_ends(a_list))
