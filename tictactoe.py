9c764f6de18da890881a540de5d4c13a1f874cc0
peteykienzzz <pmatczak2@gmail.com>
2022-04-21 18:36:58 -0700

    getBlankBoard function

--- tictactoe.py
+++ tictactoe.py
@@ -7,15 +7,15 @@ def main():
     """Returns a game of tic-tac-toe."""
     print('Welcome to tic-tac-toe!')
     gameBoard = getBlankBoard()  # Create a TTT board dictionary.
-    currentPlayer, NextPlayer = X, O  # X goes first, O goes next.
+    currentPlayer, nextPlayer = X, O  # X goes first, O goes next.
 
     while True:
         print(getBoardStr(gameBoard))  # Display the board on the screen.
 
-        # Keep asking the player until they enter a numbre 1-9;
+        # Keep asking the player until they enter a number 1-9;
         move = None
         while not isValidSpace(gameBoard, move):
-            print(f'What is {currentPlayer}\'s move? (1-9)')
+            print(f"What is {currentPlayer}\'s move? (1-9)")
             move = input()
         updateBoard(gameBoard, move, currentPlayer)  # Make the move.
 
@@ -28,13 +28,51 @@ def main():
             print(getBoardStr(gameBoard))
             print('The game is a tie!')
             break
-        currentPlayer, nextPlayer = nextPlayer, currentPlayer  # Swap turns
+        currentPlayer, nextPlayer = nextPlayer, currentPlayer # Swap turns
     print('Thanks for playing!')
 
 def getBlankBoard():
     """Create a new, blank tic-tac-toe board."""
-    board = {}  # The board is represented as a Python dicitnary.
+    board = {}  # The board is represented as a Python dictionary.
     for space in ALL_SPACES:
         board[space] = BLANK  # All spaces start as blank.
     return board
 
+def getBoardStr(board):
+    """Return a text-representation of the board."""
+    return f'''
+      {board['1']}|{board['2']}|{board['3']} 1 2 3
+      -+-+-
+      {board['4']}|{board['5']}|{board['6']} 4 5 6
+      -+-+-
+      {board['7']}|{board['8']}|{board['9']} 7 8 9'''
+
+def isValidSpace(space):
+    return space in ALL_SPACES
+
+def isWinner(board, player):
+    """Return True if plauer is a winner on this TTTBoard."""
+    b, p = board, player  # Shorter names as "syntactic sugar".
+    # Check for 3 marks across the 3 rows, 3 columns, and 2 diagonals.
+    return ((b['1'] == b['2'] == b['3'] == p) or  # Across the top
+            (b['4'] == b['5'] == b['6'] == p) or  # Across the middle
+            (b['7'] == b['8'] == b['9'] == p) or  # Across the bottom
+            (b['1'] == b['4'] == b['7'] == p) or  # Down the left
+            (b['2'] == b['5'] == b['8'] == p) or  # Down the middle
+            (b['3'] == b['6'] == b['9'] == p) or  # Down the right
+            (b['3'] == b['5'] == b['7'] == p) or  # Diagonal
+            (b['1'] == b['5'] == b['9'] == p))  # Diagonal
+
+def isBoardFull(board):
+    """Return True if every space on the board has been taken."""
+    for space in ALL_SPACES:
+        if board[space] == BLANK:
+            return  # If a single space is blank, return False
+    return True  # No spaces are blank, so return True.
+
+def updateBoard(board, space, mark):
+    """Set the space on theh board to mark."""
+    board[space] = mark
+
+if __name__ == "__main__":
+    main()
